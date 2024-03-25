077a65daf87cc1d6f37c43e2f1fe4aa889056c98
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-03-25 16:16:56 +0900

    fixup! Run check.rb on CI

--- client/rb_client/check.rb
+++ client/rb_client/check.rb
@@ -6,18 +6,3 @@ puts `gem list`
 # Check environment variables
 puts "Environment variables:"
 puts ENV.to_h
-
-# Check existence and content of important files
-files_to_check = [
-  "/path/to/file1",
-  "/path/to/file2",
-# add more file paths here
-]
-files_to_check.each do |file|
-  if File.exist?(file)
-    puts "Content of #{file}:"
-    puts File.read(file)
-  else
-    puts "File #{file} does not exist"
-  end
-end
